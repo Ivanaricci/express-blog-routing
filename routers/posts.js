575cfba9f548739cd2d7a6614657ff4a1f835b8e
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-17 15:34:58 +0200

    [UPD] Add index

--- routers/posts.js
+++ routers/posts.js
@@ -3,3 +3,10 @@ const express = require('express');
 
 // importo la classe router
 const router = express.Router();
+
+
+// index
+router.get('/', (req, res) => {
+    res.send('Lista dei post')
+})
+
