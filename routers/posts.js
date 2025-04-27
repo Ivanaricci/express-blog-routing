0f66ac1d9fb246f112bcc96898ced0fbf357c6a2
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-27 19:59:32 +0200

    [UPD] Sostituisco alle arrow function i metodi del controller

--- routers/posts.js
+++ routers/posts.js
@@ -9,35 +9,23 @@ const postController = require('../controllers/postController.js');
 
 
 // index
-router.get('/', (req, res) => {
-    res.send('Lista dei post')
-})
+router.get('/', postController.index)
 
 // show
-router.get('/:id', (req,res) =>{
-    res.send(`Dettaglio del post: ${req.params.id}`)
-})
+router.get('/:id', postController.show)
 
 // store
-router.post('/', (req,res) =>{
-    res.send('inserimento nuovo post')
-})
+router.post('/', postController.store)
 
 // update
-router.put('/:id', (req,res) => {
-    res.send(`Modifica totale del post ${req.params.id}`)
-})
+router.put('/:id', postController.update)
 
 
 // modify
-router.patch('/:id', (req,res) => {
-    res.send(`Modifica parziale del post ${req.params.id}`)
-})
+router.patch('/:id', postController.modify)
 
 // destroy
-router.delete('/:id', (req,res) =>{
-    res.send(`Cancellazione del post ${req.params.id}`)
-})
+router.delete('/:id', postController.destroy)
 
 // esporto il router dei post
 module.exports = router;
